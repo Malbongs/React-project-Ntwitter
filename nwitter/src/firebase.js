b210ab1b61128db7f85ed4fc57e548f0876e09dc
Malbongs <qothgus13@naver.com>
2022-10-26 23:03:24 +0900

    ㅍㅡ로젝트 설정

---
+++ nwitter/src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyATiZWjsHFDu7D0v2b59Ky1WHMkVr9bgcM",
+    authDomain: "ntwitter-88d16.firebaseapp.com",
+    projectId: "ntwitter-88d16",
+    storageBucket: "ntwitter-88d16.appspot.com",
+    messagingSenderId: "377165744990",
+    appId: "1:377165744990:web:351d2186f5059283405dcf"
+  };
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
