4446a295d1b142162c1e58da616477a7fa026e9e
Malbongs <qothgus13@naver.com>
2022-10-27 16:06:35 +0900

    key Value setting

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATiZWjsHFDu7D0v2b59Ky1WHMkVr9bgcM",
-    authDomain: "ntwitter-88d16.firebaseapp.com",
-    projectId: "ntwitter-88d16",
-    storageBucket: "ntwitter-88d16.appspot.com",
-    messagingSenderId: "377165744990",
-    appId: "1:377165744990:web:351d2186f5059283405dcf"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID
   };
 
 const app = initializeApp(firebaseConfig);
